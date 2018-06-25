288dcbeff37bab0ede10dcf65345708f92cdf59f
chola <chola@apples-iMac-5.local>
2018-06-25 15:11:51 +0400

    Initial commit

---
+++ Example/CRDatepicker/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CRDatepicker
+//
+//  Created by chola on 06/25/2018.
+//  Copyright (c) 2018 chola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
